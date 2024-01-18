542e142b229118d60e97c0fc4c189efc19c95596
RPNSammieJee <aigbornajohnrpnsamuel@gmail.com>
2024-01-18 02:45:24 +0100

    Update pre.py

--- pre.py
+++ pre.py
@@ -3,7 +3,6 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 import pickle
-import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import warnings
